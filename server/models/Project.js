f19d5233d24a96d5df37d7df84610adb7cdda7fe
renmission <missionrenjr@gmail.com>
2023-05-01 22:46:45 +0800

    Project and client query

---
+++ server/models/Project.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  phone: { type: String, enum: ["Not Started", "In Progress", "Completed"] },
+  clientId: { type: mongoose.Schema.Types.ObjectId, ref: "Client" },
+});
+
+module.exports = mongoose.model("Project", ProjectSchema);
